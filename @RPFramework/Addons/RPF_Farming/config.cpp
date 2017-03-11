ebb4c7eb9dab41245b484e896b9a9c66fa4dcf39
TJohnsonCZG <combatzonegaming.com@gmail.com>
2017-03-10 19:59:12 -0500

    Server Files Uploaded

---
+++ @RPFramework/Addons/RPF_Farming/config.cpp
@@ -0,0 +1,23 @@
+class CfgPatches
+{
+	class RPF_Farming
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"RPF_General"};
+	};
+};
+class CfgVehicles {
+	class RPF_PhysBase;
+	class RPF_Plant_Olive: RPF_PhysBase {
+		scope = 2;
+		model = "A3\plants_f\Bush\b_FicusC1s_F.p3d";
+		displayName = "Olive Plant";
+	};
+	class RPF_Plant_Poppy: RPF_PhysBase {
+		scope = 2;
+		model = "A3\plants_f\Bush\b_NeriumO2s_F.p3d";
+		displayName = "Poppy Plant";
+	};
+};
